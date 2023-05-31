72a8b4b6130a39c7e107d8ab93c431ecea9716c2
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-05-31 09:28:37 +0300

    completed lab

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+song.count
+
+# => 100
+
+song.artists
+#["E-Sir", "Nelly" "Off-set"]
+
+song.genres 
+# => [" Oldskool", "Blues" "Rap"]
+
+song.genre_count
+return{"oldskool"=> 6, "Blues=> 3" "trap" =>}
+
+
+song.artist.count
+# => {"E-Sir"=> 40, "Nelly" =>30 "Off-set" =>30}
